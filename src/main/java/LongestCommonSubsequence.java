a678347ef643e19fa0beaf0e2f7ad1ee25af9a7d
Zulfiquer Rhaman <z_rhaman@apple.com>
2017-11-05 09:25:10 -0800

    Added longest common subsequence length

---
+++ src/main/java/LongestCommonSubsequence.java
@@ -0,0 +1,38 @@
+/**
+ * Created by zulfiquer on 11/4/17.
+ */
+public class LongestCommonSubsequence {
+
+    public int findLength(String str1, String str2) {
+
+        if ( str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
+            return 0;
+        }
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        int[][] lcs = new int[len1  +1 ][len2 + 1];
+
+        for (int i = 0 ; i <= str1.length(); i++) {
+            for (int j = 0; j <= str2.length(); j++) {
+
+                if ( i == 0 || j == 0) {
+                    lcs[i][j] = 0;
+                }
+
+                else if (str1.charAt(i-1) == str2.charAt(j-1)) {
+                    lcs[i][j] = lcs[i-1][j-1] + 1;
+                }
+
+                else {
+                    lcs[i][j] = lcs[i][j-1] > lcs[i-1][j] ? lcs[i][j-1] : lcs[i-1][j];
+                }
+            }
+        }
+
+
+        return lcs[len1][len2];
+    }
+
+}
