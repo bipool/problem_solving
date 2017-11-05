d67e559a049d51d1a2b0b29fae8301e0dd01bec5
Zulfiquer Rhaman <z_rhaman@apple.com>
2017-11-05 11:14:56 -0800

    find sequence

---
+++ src/main/java/EditDistance.java
@@ -0,0 +1,5 @@
+/**
+ * Created by zulfiquer on 11/5/17.
+ */
+public class EditDistance {
+}
--- src/main/java/LongestCommonSubsequence.java
+++ src/main/java/LongestCommonSubsequence.java
@@ -3,17 +3,11 @@
  */
 public class LongestCommonSubsequence {
 
-    public int findLength(String str1, String str2) {
-
-        if ( str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
-            return 0;
-        }
-
+    private int[][] computeDpTable(String str1, String str2) {
         int len1 = str1.length();
         int len2 = str2.length();
 
         int[][] lcs = new int[len1  +1 ][len2 + 1];
-
         for (int i = 0 ; i <= str1.length(); i++) {
             for (int j = 0; j <= str2.length(); j++) {
 
@@ -31,8 +25,52 @@ public class LongestCommonSubsequence {
             }
         }
 
+        return lcs;
+    }
 
-        return lcs[len1][len2];
+    public int findLength(String str1, String str2) {
+
+        if ( str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
+            return 0;
+        }
+
+        int[][] lcs = computeDpTable(str1, str2);
+
+        return lcs[str1.length()][str2.length()];
+    }
+
+
+
+    public String findSequence(String str1, String str2) {
+        if ( str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
+            return null;
+        }
+
+        int[][] lcs = computeDpTable(str1, str2);
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        int length = lcs[len1][len2];
+
+        StringBuilder sb = new StringBuilder();
+        while (length != 0) {
+            if (lcs[len1-1][len2-2] == length-1) {
+                sb.insert(0,str1.charAt(len1-1));
+                length--;
+                len1--;
+                len2--;
+            }
+
+            else if (lcs[len1][len2-1] == length) {
+                len2--;
+            } else {
+                len1--;
+            }
+        }
+
+
+        return sb.toString();
     }
 
 }
